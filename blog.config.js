447c795682fbb4ced76244f68f4cdadc7e9f44af
Axis <59349697+AxisZql@users.noreply.github.com>
2026-02-18 22:02:46 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'axiszql', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '被宫崎英高拷打的底层褪色者!🫠🥹',  // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '成果与代价密不可分！',  // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://axiszql.cn', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || "axiszql's learning note", // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
